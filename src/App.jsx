0acfabefa93245c8561e6ba596849402c0665ea0
aulafirdaus <aulafirdaus2945@gmail.com>
2024-02-12 13:35:58 +0700

    try: contoh membuat class dan function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import React from 'react';
 
+class Button extends React.Component {
+    render() {
+        return (
+        <button className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white" type="submit">Buy now</button>
+        )
+    }
+}
+
+function ButtonBlack(){
+    return (
+        <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">Buy now</button>
+    )
+}
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-        <div>Hello World</div>
+        <div className='flex gap-2'>
+            <Button />
+            <Button />
+            <ButtonBlack />
+            <ButtonBlack />
+        </div>
     </div>
 )
 }
