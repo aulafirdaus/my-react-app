d412aed6ef1d33cf53783f42a4a514d5d7a0fb8c
aulafirdaus <aulafirdaus2945@gmail.com>
2024-02-12 14:09:11 +0700

    try: memberikan nilai default pada props

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 
 const Button = (props) => {
+    const {children = "...", variant = "bg-black"} = props; //destructuring (nilai default)
     return (
-        <button className={`h-10 px-6 font-semibold rounded-md ${props.variant} text-white`} type="submit">{props.children}</button>
+        <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">{children}</button>
     )
 }
 function App() {
@@ -12,8 +13,8 @@ function App() {
         <div className='flex gap-2'>
             <Button variant="bg-green-700">Login</Button>
             <Button variant="bg-sky-700">Register</Button>
-            <Button variant="bg-black">Logout</Button>
-            <Button variant="bg-red-700">Delete Account</Button>
+            <Button>Logout</Button>
+            <Button variant="bg-red-700"></Button>
         </div>
     </div>
 )
