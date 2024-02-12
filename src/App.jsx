ee3d73e2c62b557226201f3ce00e1aa0f8b08c70
aulafirdaus <aulafirdaus2945@gmail.com>
2024-02-12 14:03:40 +0700

    try: contoh penggunaan props

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 
-class Button extends React.Component {
-    render() {
-        return (
-        <button className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white" type="submit">Buy now</button>
-        )
-    }
-}
 
-function ButtonBlack(){
+const Button = (props) => {
     return (
-        <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">Buy now</button>
+        <button className={`h-10 px-6 font-semibold rounded-md ${props.variant} text-white`} type="submit">{props.children}</button>
     )
 }
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
         <div className='flex gap-2'>
-            <Button />
-            <Button />
-            <ButtonBlack />
-            <ButtonBlack />
+            <Button variant="bg-green-700">Login</Button>
+            <Button variant="bg-sky-700">Register</Button>
+            <Button variant="bg-black">Logout</Button>
+            <Button variant="bg-red-700">Delete Account</Button>
         </div>
     </div>
 )
