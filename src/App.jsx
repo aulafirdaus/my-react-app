e9aabdb69afaab1404ce520a77a15cc7c45e8ed7
aulafirdaus <aulafirdaus2945@gmail.com>
2024-02-13 10:53:22 +0700

    add: memulai implementasi atomic design

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import Button from './components/Elements/Button';
 
 function App() {
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-        <div className='flex gap-2'>
-            <Button variant="bg-green-700">Login</Button>
-            <Button variant="bg-sky-700">Register</Button>
-            <Button>Logout</Button>
-            <Button variant="bg-red-700"></Button>
+    <div className="flex justify-center min-h-screen items-center">
+        <div className="w-full max-w-xs">
+            <h1 className="text-3xl font-bold mb-2 text-blue-600">Login</h1>
+            <p className="font-medium text-slate-500 mb-8">Welcome, Please enter your details</p>
+            <form action="">
+                <div className="mb-6">
+                    <label htmlFor="" className="block text-slate-700 text-sm font-bold mb-2">Email</label>
+                    <input type="text" className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" placeholder="example@mail.com" />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="password" className="block text-slate-700 text-sm font-bold mb-2">Password</label>
+                    <input type="password" className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" placeholder="********" />
+                </div>
+                <Button classname="bg-blue-600 w-full">Login</Button>
+            </form>
         </div>
     </div>
 )
--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
-    const {children = "...", variant = "bg-black"} = props; //destructuring (nilai default)
+    const {children = "...", classname = "bg-black"} = props; //destructuring (nilai default)
     return (
-        <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">{children}</button>
+        <button className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`} type="submit">{children}</button>
     )
 }
 
